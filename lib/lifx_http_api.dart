299dd0b8c9fa3db3bd1eaf58989b89b1ea941261
Nat Green <nathanielgreen@protonmail.com>
2021-05-17 22:44:48 +0100

    Expose bulb class

--- lib/lifx_http_api.dart
+++ lib/lifx_http_api.dart
@@ -1,37 +1,4 @@
 library lifx_http_api;
 
-import 'dart:convert' show jsonDecode;
-import 'package:http/http.dart' as http;
-import './src/bulb.dart' show Bulb;
-
-class Client {
-  String apiKey;
-
-  Client(this.apiKey);
-
-  Future<List<Bulb>> listLights() async {
-    final url = Uri.parse('https://api.lifx.com/v1/lights/all');
-    final headers = {"Authorization": "Bearer ${apiKey}"};
-    final response = await http.get(url, headers: headers);
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      List<Bulb> bulbs = [];
-      data.forEach((bulb) => bulbs.add(Bulb.fromJson(bulb)));
-      return bulbs;
-    } else {
-      throw Exception('Failed to load bulb');
-    }
-  }
-
-  Future<String> setState(String uuid) async {
-    final url = Uri.parse("https://api.lifx.com/v1/lights/$uuid/state");
-    final headers = {"Authorization": "Bearer ${apiKey}"};
-    final response = await http.get(url, headers: headers);
-    if (response.statusCode == 202) {
-      final data = jsonDecode(response.body);
-      return data.status;
-    } else {
-      throw Exception('Failed to set bulb state');
-    }
-  }
-}
+export './src/bulb.dart';
+export './src/client.dart';
