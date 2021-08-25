26416ebea4bbac7801a2fdd0885b65d79bab79c7
Nathaniel Green <me@nathaniel.green>
2021-08-25 16:28:33 +0100

    Add library comment

--- lib/lifx_http_api.dart
+++ lib/lifx_http_api.dart
@@ -1,3 +1,5 @@
+/// This library provides a client to interface with the LIFX HTTP API as well
+/// as some classes for LIFX devices, device properties, and API responses.
 library lifx_http_api;
 
 export './src/client.dart';
